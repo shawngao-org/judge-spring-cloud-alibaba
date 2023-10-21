ff47b818a82941eb361f9f11b243e59eb01b8667
ShawnGao <shawngao.org@outlook.com>
2023-10-21 16:55:56 +0800

    添加Redis配置和工具类

---
+++ judge-auth/src/main/java/ltd/sgtu/judge/auth/config/RedisConfig.java
@@ -0,0 +1,47 @@
+package ltd.sgtu.judge.auth.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import ltd.sgtu.judge.auth.common.dto.InstantDeserializer;
+import ltd.sgtu.judge.auth.common.dto.InstantSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author 高新
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(java.time.Instant.class, new InstantSerializer());
+        simpleModule.addDeserializer(java.time.Instant.class, new InstantDeserializer());
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(simpleModule);
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
+}
